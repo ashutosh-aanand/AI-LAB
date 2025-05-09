b93463573ac723097651d753703112aea614d6a4
Ashutosh Anand <ashutosh.aanand@gmail.com>
2025-05-10 01:34:08 +0530

    added notes

--- 1_OpenAI_Projects/travel_assistant_bot_py/bot_v1.py
+++ 1_OpenAI_Projects/travel_assistant_bot_py/bot_v1.py
@@ -48,3 +48,25 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+"""
+This is the first version of our chatbot.
+
+Features implemented:
+1. used load_dotenv to pass the OPENAI_API_KEY as env variable and
+fetch it using os.getenv()
+2. imported openai and configured api key in it
+3. used openai to send request to the model and get response
+4. openai.chat.completions.create to create a chat completion
+
+model="gpt-4.1" is used to specify the model we want to use.
+5. used temperature, max_tokens, top_p, frequency_penalty, presence_penalty
+    and stop to control the response generation.
+
+- temperature: Controls the randomness of the output. Lower values make the output more deterministic, while higher values make it more random.
+- max_tokens: The maximum number of tokens to generate in the response. This limits the length of the output.
+- top_p: Controls the diversity of the output. It uses nucleus sampling, where the model considers only the top p% of the probability mass.
+- frequency_penalty: Controls the penalty for using frequent tokens. Higher values make the model less likely to repeat itself.
+- presence_penalty: Controls the penalty for using new tokens. Higher values make the model more likely to introduce new topics.
+
+"""
